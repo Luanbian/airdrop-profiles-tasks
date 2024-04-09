b6135469e776539db60ae92b92ec206a626322c6
Luan Almeida <luan.almeida@jbtec.com.br>
2024-04-09 15:49:38 -0300

    feat: setup material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import Home from './presentation/pages/home';
+import { ThemeSettings } from './presentation/theme/Theme';
+
+interface ProviderModernizeProps {
+    children: React.ReactNode;
+}
+
+function ProviderModernize({ children }: ProviderModernizeProps) {
+    const theme = ThemeSettings();
+
+    return (
+        <ThemeProvider
+            theme={createTheme({
+                ...theme,
+                direction: theme.direction,
+                palette: theme.palette,
+                typography: theme.typography,
+                shadows: theme.shadows,
+                shape: {
+                    borderRadius: theme.shape.borderRadius,
+                },
+            })}
+        >
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    );
+}
+
 function App() {
-    return <p>Hello world</p>;
+    return (
+        <ProviderModernize>
+            <Home />
+        </ProviderModernize>
+    );
 }
 
 export default App;
---
+++ src/presentation/theme/customizer.ts
@@ -0,0 +1,9 @@
+const customizer = {
+    activeMode: 'light',
+    activeDir: 'ltr',
+    activeTheme: 'DEFAULT_THEME',
+    borderRadius: 8,
+    isCardShadow: true,
+};
+
+export default customizer;
