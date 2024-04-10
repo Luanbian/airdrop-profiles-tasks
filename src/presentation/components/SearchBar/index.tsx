f63adb33ab3180603d26ea7dd00d2ba09350623b
Luan Almeida <luan.almeida@jbtec.com.br>
2024-04-09 22:24:18 -0300

    feat: create search bar

---
+++ src/presentation/components/SearchBar/index.tsx
@@ -0,0 +1,73 @@
+import { Button, InputBase, alpha, styled } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
+
+interface SearchBarProps {
+    onSubmit: (value: string) => void;
+}
+
+export default function SearchBar({ onSubmit }: SearchBarProps) {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = () => {
+        onSubmit(inputValue);
+    };
+
+    return (
+        <Search>
+            <SearchIconWrapper>
+                <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder='Search…'
+                inputProps={{ 'aria-label': 'search' }}
+            />
+            <Button onClick={handleSubmit}>Search</Button>
+        </Search>
+    );
+}
--- src/presentation/pages/home.tsx
+++ src/presentation/pages/home.tsx
@@ -1,11 +1,17 @@
 import { Typography } from '@mui/material';
+import SearchBar from '../components/SearchBar';
 
 export default function Home() {
+    const handleSearch = (value: string) => {
+        console.log(value);
+    };
+
     return (
         <div>
             <Typography variant='body1' sx={{ marginTop: '3%' }}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
             </Typography>
+            <SearchBar onSubmit={handleSearch} />
         </div>
     );
 }
